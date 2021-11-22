2dbd90397f8a363a2c8ad81506b263c265f28bb1
Gusb <mihivanmih@gmail.com>
2021-11-22 17:49:43 +0300

    1 hour

---
+++ src/components/PostList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h3>Список пользователей</h3>
+    <PostItem
+        :post="post"
+        v-for="post in posts"
+    />
+  </div>
+
+</template>
+
+<script>
+import PostItem from "@/components/PostItem";
+export default {
+  components: {PostItem},
+  props: {
+    posts: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/UI/MyInput.vue
@@ -0,0 +1,18 @@
+<template>
+  <input type="text">
+</template>
+
+<script>
+export default {
+  name: "MyInput"
+}
+</script>
+
+<style scoped>
+input {
+  width: 100%;
+  border: 1px solid #ccc;
+  padding: 10px;
+  margin-bottom: 20px;
+}
+</style>
