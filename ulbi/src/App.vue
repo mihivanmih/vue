738361a53d71e7aee649a50846348ceb33147868
Gusb <mihivanmih@gmail.com>
2021-11-22 18:02:38 +0300

    new structure

---
+++ ulbi/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="app">
+    <PostFom
+        @create="createPost"
+    />
+    <PostList
+        :posts="posts"
+    />
+  </div>
+</template>
+
+<script>
+import PostFom from "@/components/PostFom"
+import PostList from "@/components/PostList"
+
+export default {
+  components: {
+    PostList, PostFom
+  },
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript 1', body: 'Описание поста 1'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'}
+      ]
+    }
+  },
+  methods: {
+    createPost(post) {
+      this.posts.push(post)
+    }
+  }
+}
+</script>
+
+<style>
+*, body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+.app {
+  padding: 20px;
+}
+</style>
---
+++ ulbi/src/components/UI/MyButton.vue
@@ -0,0 +1,24 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "MyButton"
+}
+</script>
+
+<style scoped>
+button {
+  padding: 10px 15px;
+  background: none;
+  border: 1px solid #ccc;
+  cursor: pointer;
+}
+
+button:hover {
+  background: #ddd;
+}
+</style>
